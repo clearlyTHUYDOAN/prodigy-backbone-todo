c9c0aa6f5fbeaf2e59408794364e0e5bb50a8b9a
Thuy <clearlythuydoan@gmail.com>
2017-06-21 15:48:09 -0400

    iterating over collection successful but dynamic rendering not

--- js/views/TodoList.js
+++ js/views/TodoList.js
@@ -6,14 +6,19 @@ const TodoListView = Backbone.View.extend({
     },
     render: function() {
         // Render each item in collection
-        console.log(this.collection);
-        this.collection.forEach(this.renderTodo, this); // what's with the this
-        return this; // what's with the this
+        // console.log(this.collection.models[1].attributes);
+        this.collection.forEach(this.renderTodo, this); // what's with the this?
+        return this; // what's with the this?
     },
     renderTodo: function (todoItem) {
-        console.log('renderTodo is firing');
         let todoView = new TodoView({model: Todo});
-        todoView.render();
-        this.$el.append(todoView.el);
+        console.log(this.el)
+        console.log(todoView.$el.html());
+        // this.$el.append('<li class="list-group-item">Poop</li>')
+        // this.$el.append(todoView.$el.html());
+        this.$el.append('<li class="list-group-item">' + 
+                '<input type="checkbox" value="on"></input>' +
+                '<label class="">Poop</label>' + // Needs to be dynamic
+            '</li>')
     }
 })
