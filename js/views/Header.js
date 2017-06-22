e63c80d77f133d318dc7b5bf1b11bce1b36d0069
Thuy <clearlythuydoan@gmail.com>
2017-06-22 17:08:15 -0400

    remove comment

--- js/views/Header.js
+++ js/views/Header.js
@@ -2,6 +2,6 @@ App.Views.Header = Backbone.View.extend({
     el: '#header', 
     template: _.template($('#app').html()),
     render: function() {
-        this.$el.html(this.template()); // find template by an identifier and stick into el
+        this.$el.html(this.template());
     }
 });
