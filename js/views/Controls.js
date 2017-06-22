39d5aa356a3ac902e335f0f1c69fe1df8cfafee4
Thuy <clearlythuydoan@gmail.com>
2017-06-21 20:48:28 -0400

    set up select menu for filtering

--- js/views/Controls.js
+++ js/views/Controls.js
@@ -2,10 +2,15 @@ App.Views.Controls = Backbone.View.extend({
     el: '#controls',
     template: _.template($('#filter').html()),
     events: {
-        'click button': 'clearTodos'
+        'click button': 'clearTodos',
+        'change select': 'test',
     },
+    // ALL EVENTS FIRE TWICE?
     clearTodos: function() {
-        this.model.clearTodos();
+        // this.model.clearTodos();
+    },
+    test: function(e) { // test select menu 
+        console.log(e.target.value);
     },
     render: function() {
         this.$el.html(this.template()); // find template by an identifier and stick into el
