ea85cf9a9e4bb09ecb003ccea1e3ee680dec35d6
Thuy <clearlythuydoan@gmail.com>
2017-06-23 10:58:25 -0400

    test backbone router by adding paths

--- js/index.js
+++ js/index.js
@@ -7,6 +7,26 @@ const App = {
 
 $(document).ready(function() {
 
+    App.Router = Backbone.Router.extend({
+        routes: {
+            '': 'index', // root route
+            'show/:id': 'show', // hash route with dynamic params
+            '*backup': 'backup', // in case nothing is specified - need the star
+        },
+        index: function() {
+            $(document.body).append("Index route has been called...");
+        },
+        show: function(id) {
+            $(document.body).html("Show route has been called with id equal to " + id + ".");
+        },
+        backup: function(backup) {
+            $(document.body).html("This route is not handled. You tried to access " + backup + ".");
+        }
+    });
+
+    new App.Router;
+    Backbone.history.start(); // monitors hash changes in url
+
     // Render app
     const appView = new App.Views.Header();
     appView.render();
---
+++ js/router.js
@@ -0,0 +1,11 @@
+// (function() {
+
+//     App.Router = Backbone.Router.extend({
+//         routes: {
+//             '': 'index',
+//         },
+//         index: function() {
+//             $(document.body).append("Index route has been called...");
+//         }
+//     });
+// })
